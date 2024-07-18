5f08a313934ebae419e75d49829092699d004c68
Ashwin <ashwinjain101010@gmail.com>
2024-07-18 16:32:22 +0530

    add map

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 import '../Styles/contactus.css';
 
 function ContactUs() {
@@ -9,8 +12,16 @@ function ContactUs() {
             <p className='secondary'>Get in touch</p>
         </div>
         <div className="location-boxes">
-            <div className="map">
-
+            <div className="map-box">
+                <MapContainer className='map' center={[28.584791140538854, 77.16380898471955]} zoom={14} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[28.584791140538854, 77.16380898471955]}>
+                        <Popup>Right Here!</Popup>
+                    </Marker>
+                </MapContainer>
             </div>
             <div className="info-box">
                 <div className="info-content">
