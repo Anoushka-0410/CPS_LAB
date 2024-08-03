07ce43204cf3a8934f40ceb5ff10a7a280573cda
Ashwin <ashwinjain101010@gmail.com>
2024-08-03 15:00:16 +0530

    refactor code

--- src/pages/Publications.jsx
+++ src/pages/Publications.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import publicationsData from '../assets/publicationsdata';
 import { AgGridReact } from 'ag-grid-react';
 import "ag-grid-community/styles/ag-grid.css";
@@ -8,20 +8,60 @@ import '../Styles/publications.css';
 import parse from 'html-react-parser';
 import TitleCard from '../Components/TitleCard'
 
+//common options for all AG grid columns except Year
+const commonColumnOptions = { 
+  filter: true, 
+  floatingFilter: true, 
+  autoHeight: true, 
+  resizable: false
+};
+
+//disable column autosizing 
+const autoSizeStrategy = {
+  type: 'fitCellContents'
+};
+
+
 function Publications() {
+  const tableRef = useRef(null);
   const [rowData, setRowData] = useState(publicationsData);
   const [columns, setColumns] = useState([
-    { field: "Publication", filter: true, floatingFilter: true, autoHeight: true, cellRenderer: ({value}) => <div className='publication-cell'>{parse(value)}</div>},
-    { field: "Publisher", filter: true, floatingFilter: true, cellRenderer: ({value}) => <div className='publisher-cell'>{parse(value)}</div>},
-    { field: "Author(s)", filter: true, floatingFilter: true, cellRenderer: ({value}) => <div className='authors-cell'>{parse(value)}</div>},
-    { field: "Keywords", filter: true, floatingFilter: true, cellRenderer: ({value}) => <div className='keywords-cell'>{parse(value)}</div>},
-    { field: "Year"},
+    { 
+      field: "Publication", 
+      ...commonColumnOptions,
+      cellRenderer: ({value}) => <div className='publication-cell'>{parse(value)}</div>
+    },
+    { 
+      field: "Publisher", 
+      ...commonColumnOptions,
+      cellRenderer: ({value}) => <div className='publisher-cell'>{parse(value)}</div>
+    },
+    { 
+      field: "Author(s)", 
+      ...commonColumnOptions,
+      cellRenderer: ({value}) => <div className='authors-cell'>{parse(value)}</div>
+    },
+    { 
+      field: "Keywords", 
+      ...commonColumnOptions,
+      cellRenderer: ({value}) => <div className='keywords-cell'>{parse(value)}</div>
+    },
+    { 
+      field: "Year"
+    },
   ]);
   return (
     <div className='publications'>
         <TitleCard title='Publications' backgroundImage={banner} />
         <div className="table ag-theme-quartz">
-          <AgGridReact rowData={rowData} columnDefs={columns} pagination={true}/>
+          <AgGridReact 
+            ref={tableRef}
+            rowData={rowData} 
+            columnDefs={columns} 
+            pagination={true} 
+            autoSizeStrategy={autoSizeStrategy}
+            suppressMovableColumns={true}
+          />
         </div>
     </div>
   )
