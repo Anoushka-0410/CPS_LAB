ba890157265e0070f6940da62234aa531919588f
Anoushka Singh <anshka42@gmail.com>
2024-09-01 23:33:09 +0530

    navbar changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,29 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [hoverIndex, setHoverIndex] = useState(0);
+  const navItems = [
+    { to: "/", text: "Home", hasDropdown: false },
+    { to: "research", text: "Research Focus", hasDropdown: false },
+    { to: "publications", text: "Publications", hasDropdown: false },
+    {
+      to: "presentmembers",
+      text: "People",
+      hasDropdown: true,
+      dropdownItems: [
+        { to: "presentmembers", text: "Present Lab Members" },
+        { to: "pastmembers", text: "Past Lab Members" },
+      ],
+    },
+    {
+      to: "joinus",
+      text: "Join Us",
+      hasDropdown: true,
+      dropdownItems: [{ to: "joinus", text: "Apply to CPS Lab" }],
+    },
+    { to: "contactus", text: "Contact Us", hasDropdown: false },
+  ];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -12,47 +35,62 @@ function Navbar() {
         setIsScrolled(false);
       }
     };
+    console.log(isScrolled);
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   });
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const handleMouseEnter = (index) => {
+    setHoverIndex(index);
+  };
+
+  const handleMouseLeave = (index) => {
+    setHoverIndex();
+  };
 
   return (
-    // <div className="nav-wrapper">
     <div className={`navbar ${isScrolled ? "scrolled" : ""} `}>
-      {/* <div className="navbar-wrapper"> */}
       <img
         src="https://cps.iic.ac.in/wp-content/uploads/2022/05/cypsi_lab_logo-800x375.png"
         alt=""
         className="nav-img"
       />
-      <div className="innav">
-        <Link to="/" className="nav-link">
-          <div className="nav-element">Home</div>
-        </Link>
-        <Link to="research" className="nav-link">
-          <div className="nav-element">Research Focus</div>
-        </Link>
-        <Link to="publications" className="nav-link">
-          <div className="nav-element">Publications</div>
-        </Link>
-
-        <Link to="presentmembers" className="nav-link dropdown">
-          <div className="nav-element">People</div>
-          <div className="dropdown-content">
-            <div className="dropdown-link">Present Lab Members</div>
-            <div className="dropdown-link">Past Lab Members</div>
+      <div className="innav" onMouseLeave={handleMouseLeave}>
+        {navItems.map((item, index) => (
+          <div
+            key={index}
+            className={`nav-item ${item.hasDropdown ? "dropdown" : ""}`}
+          >
+            <Link to={item.to} className="nav-link">
+              <div
+                className={`nav-element ${
+                  activeIndex === index ? "active" : ""
+                }`}
+                onMouseEnter={() => handleMouseEnter(index)}
+                onClick={() => handleClick(index)}
+              >
+                {item.text}
+              </div>
+              {item.hasDropdown && (
+                <div className="dropdown-content">
+                  {item.dropdownItems.map((dropdownItem, i) => (
+                    <Link
+                      key={i}
+                      to={dropdownItem.to}
+                      className="dropdown-link"
+                    >
+                      {dropdownItem.text}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </Link>
           </div>
-        </Link>
-        <Link to="joinus" className="nav-link dropdown">
-          <div className="nav-element">Join Us</div>
-          <div className="dropdown-content">
-            <div className="dropdown-link">Apply to CPS Lab</div>
-          </div>
-        </Link>
-        <Link to="contactus" className="nav-link">
-          <div className="nav-element">Contact Us</div>
-        </Link>
+        ))}
       </div>
     </div>
     // </div>
